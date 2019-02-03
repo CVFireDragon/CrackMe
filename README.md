15972ceb8a8cb22ee86e16cdd13b4d242ca0b390
Luke <39099814+CVFireDragon@users.noreply.github.com>
2019-02-02 21:05:36 -0500

    Upload my First C# GUI CrackMe

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CrackMe
 A few CrackMes that I wrote in a Few Coding Languages
+
+Icons (If used) are from Icons8
---
+++ Windows/GUI/C#/EzGuiCM/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EzGuiCM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Contains("MrSaturn"))
+            {
+                Form3 CorrectForm = new Form3();
+                CorrectForm.Show();
+            }
+            else
+            {
+                Form2 InCorrectForm = new Form2();
+                InCorrectForm.Show();
+            }
+        }
+    }
+}
